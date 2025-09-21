e5fb8a165fc5b5eaee8e445b8fddb7d439fdf636
Cyrus Warym Aceveda <acevedazyrus2165@gmail.com>
2025-09-21 20:52:14 +0800

    test

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mina',
+    'username'	=> 'sql12798229',
+    'password'	=> 'hv1UtHgYJt',
+    'database'	=> 'sql12798229',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
